45ef296e1774f7d16f8454de606ba6d62d610bae
Sartor <sartor@syneforge.com>
2025-08-12 18:35:54 +0300

    No HMR, but autorefresh working consistent. Need security breach check

--- back/server.ts
+++ back/server.ts
@@ -1,18 +1,27 @@
-import { serve } from "bun"
-
-import pageIndex from "../dist/index.html"
+import { serve, file } from "bun"
 
 const server = serve({
     port: 3000,
     development: true,
     routes: {
-        "/": pageIndex,
         "/api/hello": async req => {
             return new Response(JSON.stringify({message: "Hello World from Bun!"}))
         },
     },
 
     async fetch(req) {
+        const path = new URL(req.url).pathname
+
+        // Special case - index page
+        if (path === "/" || (!path.startsWith("/assets") && !path.startsWith("/api"))) {
+            return new Response( file(`dist/index.html`))
+        }
+
+        // All other assets, that really exists and path not started with api
+        if (path.startsWith("/assets")) {
+            return new Response( file(`dist/${path}`))
+        }
+
         // Return 404 for unmatched routes
         return new Response("Not Found", {status: 404})
     },
